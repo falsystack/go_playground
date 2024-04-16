e446ef4d941cca73037102afe5a0ff619aba56b4
falsystack <falsystack@gmail.com>
2024-04-16 21:15:54 +0900

    feat: Router Grouping

--- go-gin/main.go
+++ go-gin/main.go
@@ -43,11 +43,12 @@ func main() {
 
 	r := gin.Default() // default ルータの初期化
 
-	r.GET("/items", itemController.FindAll)
-	r.GET("/items/:id", itemController.FindById)
-	r.POST("/items", itemController.Create)
-	r.PUT("/items/:id", itemController.Update)
-	r.DELETE("/items/:id", itemController.Delete)
+	itemRouter := r.Group("/items")
+	itemRouter.GET("/", itemController.FindAll)
+	itemRouter.POST("/", itemController.Create)
+	itemRouter.GET("/:id", itemController.FindById)
+	itemRouter.PUT("/:id", itemController.Update)
+	itemRouter.DELETE("/:id", itemController.Delete)
 
 	r.Run("localhost:8080") // 0.0.0.0:8080 でサーバーを立てます。
 }
