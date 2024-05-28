733a317b6abb40d6e39296ca3ed4296691b870e2
falsystack <falsystack@gmail.com>
2024-05-29 01:00:58 +0900

    JSON Transfer

--- go-web-tucker/web1/main.go
+++ go-web-tucker/web1/main.go
@@ -1,24 +1,52 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"time"
 )
+
+type User struct {
+	FirstName string    `json:"first_name"`
+	LastName  string    `json:"last_name"`
+	Email     string    `json:"email"`
+	CreatedAt time.Time `json:"created_at"`
+}
 
 type fooHandler struct{}
 
 func (f *fooHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello Foo!")
+	user := &User{}
+	err := json.NewDecoder(r.Body).Decode(user)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprint(w, "Bad request", err)
+		return
+	}
+	user.CreatedAt = time.Now()
+
+	jsonBin, _ := json.Marshal(user)
+	w.WriteHeader(http.StatusOK)
+	w.Header().Add("Content-Type", "application/json")
+	fmt.Fprint(w, string(jsonBin))
 }
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "Hello World")
 	})
-	http.HandleFunc("/bar", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Hello Bar!")
-	})
+	http.HandleFunc("/bar", barHandler)
 	http.Handle("/foo", &fooHandler{})
 
 	http.ListenAndServe(":3000", nil)
+}
+
+func barHandler(w http.ResponseWriter, r *http.Request) {
+	name := r.URL.Query().Get("name")
+	name2 := r.FormValue("name")
+	if name == "" {
+		name = "World"
+	}
+	fmt.Fprintf(w, "Hello %s, %s!", name, name2)
 }
