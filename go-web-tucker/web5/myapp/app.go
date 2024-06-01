42c17333447c9872cebd2860a0c11ced19a5bd23
falsystack <falsystack@gmail.com>
2024-06-01 15:27:46 +0900

    etc

--- go-web-tucker/web5/myapp/app.go
+++ go-web-tucker/web5/myapp/app.go
@@ -1,18 +1,46 @@
 package myapp
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
 	"net/http"
+	"time"
 )
+
+type User struct {
+	ID        int       `json:"id"`
+	FirstName string    `json:"first_name"`
+	LastName  string    `json:"last_name"`
+	Email     string    `json:"email"`
+	CreatedAt time.Time `json:"created_at"`
+}
 
 func NewHandler() http.Handler {
 	mux := mux.NewRouter()
 
 	mux.HandleFunc("/", indexHandler)
-	mux.HandleFunc("/users", usersHandler)
+	mux.HandleFunc("/users", usersHandler).Methods("GET")
+	mux.HandleFunc("/users", createUserHandler).Methods("POST")
 	mux.HandleFunc("/users/{id:[0-9]+}", getUserInfoHandler)
 	return mux
+}
+
+func createUserHandler(w http.ResponseWriter, r *http.Request) {
+	user := &User{}
+	err := json.NewDecoder(r.Body).Decode(user)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprint(w, err)
+		return
+	}
+
+	user.ID = 2
+	user.CreatedAt = time.Now()
+	w.WriteHeader(http.StatusCreated)
+	// Marsha : struct -> []byte
+	data, _ := json.Marshal(user)
+	fmt.Fprint(w, string(data))
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
--- go-web-tucker/web5/myapp/app_test.go
+++ go-web-tucker/web5/myapp/app_test.go
@@ -1,10 +1,13 @@
 package myapp
 
 import (
+	"encoding/json"
 	"github.com/stretchr/testify/assert"
 	"io"
 	"net/http"
 	"net/http/httptest"
+	"strconv"
+	"strings"
 	"testing"
 )
 
@@ -52,4 +55,30 @@ func TestGetUserInfo(t *testing.T) {
 	assert.Equal(http.StatusOK, resp.StatusCode)
 	data, _ := io.ReadAll(resp.Body)
 	assert.Contains(string(data), "User ID:89")
+}
+
+func TestCreateUser(t *testing.T) {
+	// given
+	assert := assert.New(t)
+
+	ts := httptest.NewServer(NewHandler())
+	defer ts.Close()
+
+	body := `{"first_name": "ky", "last_name":"Yun", "email":"test@test.com"}`
+
+	// when
+	resp, err := http.Post(ts.URL+"/users", "application/json", strings.NewReader(body))
+
+	// then
+	assert.NoError(err)
+	assert.Equal(http.StatusCreated, resp.StatusCode)
+
+	user := &User{}
+	err = json.NewDecoder(resp.Body).Decode(user)
+	assert.NoError(err)
+	assert.Equal("2", user.ID)
+
+	resp, err = http.Get(ts.URL + "/users/" + strconv.Itoa(user.ID))
+	assert.NoError(err)
+	assert.Equal(http.StatusOK, resp.StatusCode)
 }
