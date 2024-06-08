0594d850317da41e62d198595884227596c3dc89
falsystack <falsystack@gmail.com>
2024-06-08 22:28:39 +0900

    feat(gitbook): Slice

--- go-tdd/gitbook/slices/sum.go
+++ go-tdd/gitbook/slices/sum.go
@@ -1,9 +1,30 @@
 package slices
 
-func Sum(numbers [5]int) int {
+func Sum(numbers []int) int {
 	var sum int
 	for _, number := range numbers {
 		sum += number
 	}
 	return sum
+}
+
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, num := range numbersToSum {
+		sums = append(sums, Sum(num))
+	}
+	return sums
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+			continue
+		}
+		tail := numbers[1:]
+		sums = append(sums, Sum(tail))
+	}
+	return sums
 }
