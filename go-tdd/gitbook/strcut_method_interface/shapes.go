66aff6c4382a28121f8d233544aeb393ff7f4f87
falsystack <falsystack@gmail.com>
2024-06-09 08:40:43 +0900

    refactor(gitbook): named test

--- go-tdd/gitbook/strcut_method_interface/shapes.go
+++ go-tdd/gitbook/strcut_method_interface/shapes.go
@@ -15,6 +15,10 @@ func (r *Rectangle) Area() float64 {
 	return r.Width * r.Height
 }
 
+func Perimeter(rec *Rectangle) float64 {
+	return 2 * (rec.Width + rec.Height)
+}
+
 type Circle struct {
 	Radius float64
 }
@@ -23,6 +27,11 @@ func (c *Circle) Area() float64 {
 	return c.Radius * c.Radius * math.Pi
 }
 
-func Perimeter(rec *Rectangle) float64 {
-	return 2 * (rec.Width + rec.Height)
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+func (t *Triangle) Area() float64 {
+	return (t.Base * t.Height) * 0.5
 }
