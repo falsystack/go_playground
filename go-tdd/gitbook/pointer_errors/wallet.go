2595061c49607ef2c24aadb29a0b4654eff77df2
falsystack <falsystack@gmail.com>
2024-06-09 14:23:07 +0900

    reafctor(gitbook): alias type

---
+++ go-tdd/gitbook/pointer_errors/wallet.go
@@ -0,0 +1,27 @@
+package pointer_errors
+
+import "fmt"
+
+type Bitcoin int
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
+type Wallet struct {
+	balance Bitcoin
+}
+
+func (w *Wallet) Deposit(amount Bitcoin) {
+	fmt.Printf("address of balance in Deposit is %v \n", &w.balance)
+
+	w.balance += amount
+}
+
+func (w *Wallet) Balance() Bitcoin {
+	return w.balance
+}
+
+func (w *Wallet) WithDraw(amount Bitcoin) {
+	w.balance -= amount
+}
