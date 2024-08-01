d27dcfb6a6938264622067879b601b0a3ecc010c
falsystack <falsystack@gmail.com>
2024-08-01 22:16:29 +0900

    go-tdd

--- go-tdd/chap14_context/context.go
+++ go-tdd/chap14_context/context.go
@@ -1,6 +1,23 @@
 package chap14_context
 
+import (
+	"context"
+	"fmt"
+	"net/http"
+)
+
 type Store interface {
-	Fetch() string
-	Cancel()
+	Fetch(ctx context.Context) (string, error)
+}
+
+func Server(store Store) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		data, err := store.Fetch(r.Context())
+
+		if err != nil {
+			return
+		}
+
+		fmt.Fprint(w, data)
+	}
 }
--- go-tdd/chap14_context/context_test.go
+++ go-tdd/chap14_context/context_test.go
@@ -1,17 +1,52 @@
 package chap14_context
 
-import "time"
+import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
 
-type SpyStore struct {
-	response  string
-	cancelled bool
-}
+func TestHandler(t *testing.T) {
+	data := "hello, world"
 
-func (s *SpyStore) Fetch() string {
-	time.Sleep(100 * time.Millisecond)
-	return s.response
-}
+	t.Run("returns data from store", func(t *testing.T) {
+		store := &SpyStore{response: data, t: t}
+		svr := Server(store)
 
-func (s *SpyStore) Cancel() {
-	s.cancelled = true
+		req := httptest.NewRequest(http.MethodGet, "/", nil)
+		res := httptest.NewRecorder()
+
+		svr.ServeHTTP(res, req)
+
+		if res.Body.String() != data {
+			t.Errorf("got %s, want %s", res.Body.String(), data)
+		}
+
+		//store.assertWasNotCancelled()
+	})
+
+	t.Run("tells store to cancel work if request is cancelld", func(t *testing.T) {
+		store := &SpyStore{
+			response: data,
+			t:        t,
+		}
+		svr := Server(store)
+
+		req := httptest.NewRequest(http.MethodGet, "/", nil)
+		cancellingCtx, cancel := context.WithCancel(req.Context())
+		time.AfterFunc(5*time.Millisecond, cancel) // その関数が5ミリ秒で呼び出されるようにスケジュール
+		req = req.WithContext(cancellingCtx)
+
+		//res := httptest.NewRecorder()
+		res := &SpyResponseWriter{}
+
+		svr.ServeHTTP(res, req)
+
+		if res.written {
+			t.Errorf("a response should not have been written")
+		}
+	})
+
 }
