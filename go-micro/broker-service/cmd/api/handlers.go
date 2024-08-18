648e2c1635909c129f67ade69e779f1a54fda48e
falsystack <falsystack@gmail.com>
2024-08-18 15:37:43 +0900

    go-micro : broker-service done

--- go-micro/broker-service/cmd/api/handlers.go
+++ go-micro/broker-service/cmd/api/handlers.go
@@ -1,26 +1,14 @@
 package main
 
 import (
-	"encoding/json"
 	"net/http"
 )
 
-type jasonResponse struct {
-	Error   bool   `json:"error"`
-	Message string `json:"message"`
-	Data    any    `json:"data,omitempty"`
-}
-
 func (c *Config) Broker(w http.ResponseWriter, r *http.Request) {
-	payload := jasonResponse{
+	payload := jsonResponse{
 		Error:   false,
 		Message: "Hit the broker",
 	}
 
-	// struct -> jason string
-	out, _ := json.MarshalIndent(payload, "", "\t")
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusAccepted)
-	w.Write(out)
+	_ = c.writeJSON(w, http.StatusOK, payload)
 }
